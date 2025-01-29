7956ce9d0c893e5a351b566581d28832d7446d47
lijunchen1965 <lchen1965@gmail.com>
2025-01-29 15:09:15 -0500

    Test streamlit

---
+++ test.py
@@ -0,0 +1,8 @@
+
+# tuple
+# set
+# dictionary 
+
+print("I am a student")
+
+print("I am a student too")
---
+++ teststreamlit.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("Hello, Streamlit!")
+st.write('This is my first Streamlit App.')
+
+
+#x = float(input("Please enter a number: "))
+#y = float(input("please enter another number: "))
+#z= x + y 
+#print(f"The sum of the two numbers is: {z}")
+#quit 
